d6af917633619e6fdbd11bccc2e81bad83614c8f
Artur Zelik <zel1916@o2.pl>
2026-01-24 15:16:56 +0100

    add example_bert.py (init)

---
+++ examples/replay_datasets/example_bert.py
@@ -0,0 +1,81 @@
+import numpy as np
+from loco_mujoco.task_factories import (
+    ImitationFactory,
+    LAFAN1DatasetConf,
+    DefaultDatasetConf,
+    AMASSDatasetConf,
+)
+import pandas as pd
+from pathlib import Path
+import os
+import loco_mujoco
+
+PROJECT_ROOT = Path(loco_mujoco.__file__).parent.parent
+EMBEDDING_DIR = os.path.join(PROJECT_ROOT, "dataset/processed_embeddings")
+INDEX_PATH = os.path.join(PROJECT_ROOT, "index.csv")
+
+# TODO: make it more scalable
+dataset_paths = [
+    "KIT/3/kick_high_left02_poses",
+    "KIT/3/jump_left02_poses",
+]
+
+
+def load_embeddings_from_files(
+    dataset_paths: list, index_path: str, embedding_dir: str
+) -> tuple:
+    """
+    Loads specific .npy embeddings for the requested dataset_paths.
+    """
+    print(f"Loading embeddings for {len(dataset_paths)} motions...")
+
+    try:
+        df = pd.read_csv(index_path)
+
+        # We strip spaces and ensure string types for safety
+        source_to_id = dict(
+            zip(df["source_path"].astype(str).str.strip(), df["new_name"].astype(str))
+        )
+
+        embeddings = []
+        texts = []
+
+        for path in dataset_paths:
+            found_id = None
+            for source, hid in source_to_id.items():
+                if path in source:
+                    found_id = hid.split(".")[0]  # "000321.npy" -> "000321"
+                    break
+
+            if not found_id:
+                print(f"[WARN] ID not found for: {path}. Using Zero Vector.")
+                embeddings.append(np.zeros(768))
+                texts.append("Unknown")
+                continue
+
+            # Load the specific .npy file
+            npy_path = os.path.join(embedding_dir, f"{found_id}_embed.npy")
+
+            if os.path.exists(npy_path):
+                emb = np.load(npy_path)
+                embeddings.append(emb)
+                texts.append(str(found_id)) # TODO: we can change to text if needed
+            else:
+                print(f"[WARN] Embedding file missing: {npy_path}")
+                embeddings.append(np.zeros(768))
+                texts.append("Missing")
+    except Exception as e:
+        print(f"Error loading embeddings: {e}")
+        return np.array([]), []
+
+    return np.array(embeddings), texts
+
+
+# 2. Load Data
+embeddings_array, text_list = load_embeddings_from_files(
+    dataset_paths, INDEX_PATH, EMBEDDING_DIR
+)
+
+print(f" embeddings shape: {embeddings_array.shape}, texts: {text_list}")
+# env.task.trajectory_handler.embeddings = aligned_embeddings
+# env.play_trajectory(n_episodes=3, n_steps_per_episode=500, render=True)
